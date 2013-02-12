1ecf4e22fcc0679b1139ebba19b6c74865512f6a
Rodrigo di Lorenzo Lopes <rodrigo.lorenzo@abril.com.br>
2013-02-12 01:27:37 -0200

    adicionado comentario no programa fizzbuzz funcional

--- Exemplos/2-container/fizzbuzz.rb
+++ Exemplos/2-container/fizzbuzz.rb
@@ -1,3 +1,5 @@
+# Ainda o problema do FizzBuzz, mas com a seguinte restrição:
+# o problema deve ser resolvido com programação funcional
 fizz = [nil, nil, 'Fizz'].cycle
 buzz = [nil, nil, nil, nil, 'Buzz'].cycle
  
