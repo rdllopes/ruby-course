65de805da0f0e99bdc71faefa2ffa871976e1be7
Rodrigo di Lorenzo Lopes <rodrigo.lorenzo@abril.com.br>
2013-02-12 01:27:08 -0200

    adicionado comentario no exemplo com classe de equivalencia

--- Exemplos/2-container/classe_de_equivalencia.rb
+++ Exemplos/2-container/classe_de_equivalencia.rb
@@ -1,3 +1,5 @@
+# Esses dois segmentos de código são "equivalentes"
+# eles agrupam os números de 1 a 100 em classes de equivalencia de 5
 hash = {}; (1..100).each{|i| hash[i%5] ||= []; hash[i%5] += [i]}; hash
 
 (1..100).group_by {|i| i%5}
