dc3346dac9f2cf69828597601c181d16de72c4b0
Rodrigo di Lorenzo Lopes <rodrigo.lorenzo@abril.com.br>
2013-02-12 12:34:07 -0200

    uma forma mais precisa de calcular derivadas

--- Exemplos/3-blocos/derivative.rb
+++ Exemplos/3-blocos/derivative.rb
@@ -5,10 +5,33 @@
 def d(f)
    lambda {|x|
      dx = 0.0000000001
-     dx = dx * x if x < 1 && 0 < x
+     dx = dx * x if x.abs < 1 && 0 < x.abs
      (f[x+dx]-f[x])/dx
    }
 end
 f = lambda {|x| x**2}
 puts d(f)[4]
 
+
+
+def lim(f,h)
+ y=h
+ rest = 1.0
+ while (true)
+   rest = rest/10
+   x = h + rest ##increase how close x is to h 1.0,0.1,0.001 etc..
+   fx=f[x] ##get f(x)
+   break if y==fx ##if we start getting the same val, stop
+   y=fx ##store the last val
+ end
+ y ##return where we stopped
+end
+
+ddx=lambda {|f|
+ lambda {|x|
+  lim(lambda{|dx| ##the limit of the derivative function
+   (f[x+dx]-f[x])/dx
+  },0) #as h approaches 0
+ }
+}
+puts ddx[f][4]
