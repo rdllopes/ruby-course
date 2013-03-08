020ba3ef3037e4a26d5cfab11fb22a5a5386aa1d
Rodrigo di Lorenzo Lopes <rodrigo.lorenzo@abril.com.br>
2013-03-08 14:01:09 -0300

    removido scripts de construcao das apresentacoes

--- src/ascii.rb
+++
@@ -1,8 +0,0 @@
-#!//Users/rlopes/.rvm/rubies/ruby-1.9.3-p194/bin/ruby
-
-require "active_support/all"
-
-ARGF.each do |line|
-  puts line.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'')  
-end
-
--- src/build_press.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-pandoc -f latex -t markdown $1 -o temp.md
-output=$(echo $1 | sed -e "s/tex$/md/") 
-./separator.rb < temp.md > $output
-mdpress -s custom $output
